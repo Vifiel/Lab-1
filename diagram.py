3bf7cf806172eb3c78fdb1b06d0a3a83d63637f7
Vifiel <ibaklanov.696@gmail.com>
2024-10-08 12:25:13 +0300

    diagram

--- diagram.py
+++ diagram.py
@@ -1,7 +1,7 @@
 from math import floor
 
 BLACK = "\033[40m"
-BLUE = "\033[41m"
+BLUE = "\033[44m"
 file = open("sequence.txt")
 s1, s2 = 0, 0
 
@@ -13,8 +13,9 @@ if __name__ == "__main__":
         s2 += abs(float(file.readline()))
     
     s = s1 + s2
-    s1 = floor(round(s1/s*100)/10)
-    s2 = floor(round(s2/s*100)/10)
+    per1, per2 = round(s1/s*100), round(s2/s*100)
+    s1 = floor(round(s1/s*100)/5)
+    s2 = floor(round(s2/s*100)/5)
     for i in range(max(s1, s2)):
         if s1 > s2:
             print(BLUE + " "*5 + BLACK + " "*10)
@@ -24,5 +25,6 @@ if __name__ == "__main__":
             s1 -= 1
             s2 -= 1
         else:
-            print(BLACK + " "*10 + BLUE + " "*5)
+            print(BLACK + " "*10 + BLUE + " "*5 + BLACK)
             s2 -= 1
+        print(f"s1({per1}%)" + "  " + f"s2({per2}%)")
