62d672fc612013b35cd2c0edd56ab7273079f3ae
Vifiel <ibaklanov.696@gmail.com>
2024-10-08 14:58:28 +0300

    animation

---
+++ dop.py
@@ -0,0 +1,18 @@
+from time import sleep
+from os import system
+
+WHITE = "\033[47m"
+BLACK = "\033[40m"
+i = 0
+frames = ["|", "\\", "-", "/"]
+
+if __name__ == "__main__":
+    for i in range(300):
+        f = i%4
+        i = i//15
+        download = f"Hacking Pentagon: {i*5}%" + "[" + WHITE + " "*(i) + BLACK + " "*(20-i) + "]" + BLACK + " "
+        print(frames[f] + "  " + download)
+        sleep(0.05)
+        print("\033[H")
+    system("cls")
+    print("Information accessed!1!1!!")
